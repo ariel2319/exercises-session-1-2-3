bda1437deef434eddb46275d271cb447b6dacb7e
areil <maldonadoaa.91@gmail.com>
2022-12-21 20:58:59 -0300

    second commit

--- src/components/RenderContact.jsx
+++ src/components/RenderContact.jsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useState} from 'react';
 import PropTypes from 'prop-types';
 import { contactData } from '../models/contact.class';
 
 
 const RenderContact = ({ contact }) => {
-  
+  const stateContact = contact.conected
+
+  const [conected, isConected] = useState(stateContact);
+
+  const changeState = () => {
+    isConected(!conected)
+  }
+
+
   return (
     <div>
       <h1 style={{textAlign: 'left'}}> CONTACT </h1>
+
       <h2 style={{textAlign: 'left'}}> Details: </h2>
+
       <h4 style={{textAlign: 'left'}}>
         Full Name: {contact?.name} {contact.lastName}
       </h4>
+
       <h4 style={{textAlign: 'left'}}>
         Email: {contact?.email}
       </h4>
 
       <h3 style={{textAlign: 'left'}}>
-        Contact <b> {contact.conected ? 'On Line' : 'Not Available'} </b>
+        Contact <b> {conected ? 'On Line' : 'Not Available'} </b>
       </h3>
+
+<button onClick={changeState}> {conected !== true ? 'Connected' : 'Disconnect'} </button>
+
       
     </div>
   );
